b0d333c94f481b7e08d0979e0ce3e1cef3ae25c8
yashicamanohar <yashicamanoharofficial@gmail.com>
2025-05-15 10:22:08 +0530

    Add files via upload

---
+++ tableswithforloop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+inthttps://www.onlinegdb.com/online_c_compiler#editor_1 main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+   for(int i=1;i<5;i++)
+   {
+       printf("%d * %d = %d\n",i,y,i*y);
+   }
+    return 0;
+}
